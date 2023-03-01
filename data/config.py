62120ab87b9a6de553c636632684c7d876f4ec7a
sachaek <sacha-ek@yandex.ru>
2023-03-01 17:32:24 +0500

    add config-constants for qiwi: QIWI_TOKEN,WALLET_QIWI,QIWI_PUBKEY

--- data/config.py
+++ data/config.py
@@ -6,6 +6,9 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+IP = env.str("IP")  # Тоже str, но для айпи адреса хоста
+QIWI_TOKEN = env.str("QIWI_TOKEN")
+WALLET_QIWI = env.str("WALLET_QIWI")
+QIWI_PUBKEY = env.str("QIWI_PUBKEY")
 
 banner_users = [1241241, 214124]
