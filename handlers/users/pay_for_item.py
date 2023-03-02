fae17a96a6333224a1622b6c535205a16bf44f0e
sachaek <sacha-ek@yandex.ru>
2023-03-02 11:11:43 +0500

    create /items handler

---
+++ handlers/users/pay_for_item.py
@@ -0,0 +1,25 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+
+from loader import dp
+
+
+@dp.message_handler(Command("items"))
+async def show_items(message: types.Message):
+    caption = """
+    Название продукта: {title}
+    <i>Описание продукта:</i>
+    {description}
+    
+    <u>Цена:</u> {price:.2f} <b>руб.</b>
+    """
+    items = []
+    for item in items:
+        await message.answer_photo(photo=item.photo_link,
+                                   caption=caption.format(
+                                       title=item.title,
+                                       description=item.description,
+                                       price=item.price,
+                                   ),
+                                   reply_markup=buy_keyboard(item)
+                                   )
