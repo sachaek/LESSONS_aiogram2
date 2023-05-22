b186058f38a9a89bff2df72089bfdf676a6d43ac
sacha_ek <sacha-ek@yandex.ru>
2023-05-22 11:46:58 +0500

    add postgresql.py

--- utils/db_api/postgressql.py
+++ utils/db_api/postgressql.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import asyncpg
+from asyncpg import Connection
 from asyncpg.pool import Pool
 
 from data import config
@@ -10,14 +11,80 @@ class Database:
     def __init__(self):
         self.pool: Union[Pool, None] = None
 
-        async def create(self):
-            self.pool = await asyncpg.create_pool(
-                user=config.DB_USER,
-                password=config.DB_PASS,
-                host="localhost",
-                port=5432,
-                database="postgres",
-                max_size=10,
-                min_size=1,
-            )
+    async def create(self):
+        self.pool = await asyncpg.create_pool(
+            user=config.DB_USER,
+            password=config.DB_PASS,
+            host="localhost",
+            port=5432,
+            database="postgres",
+            max_size=10,
+            min_size=1,
+        )
 
+    # =====================================
+    # fetch - выгрузить все;
+    # fetchval - выгрузить одно значение;
+    # fetchrow - выгрузить одно строку;
+    # execute - выполнить команду, ;
+    # =====================================
+    async def execute(self, command: str, *args,
+                      fetch: bool = False,
+                      fetchval: bool = False,
+                      fetchrow: bool = False,
+                      execute: bool = False):
+
+        async with self.pool.acquire() as connection:
+            connection: Connection  # нужно для подсказок
+            async with connection.transaction():
+                if fetch:
+                    result = await connection.fetch(command, *args)
+                elif fetchval:
+                    result = await connection.fetchval(command, *args)
+                elif fetchrow:
+                    result = await connection.fetchrow(command, *args)
+                elif execute:
+                    result = await connection.execute(command, *args)
+            return result
+
+    async def create_table(self):
+        sql = """
+        CREATE TABLE IF NOT EXISTS Users (
+        id SERIAL PRIMARY KEY,
+        full_name VARCHAR(255) NOT NULL,
+        username VARCHAR(255) NULL,
+        telegram_id BIGINT NOT NULL,            
+        );
+        """
+        await self.execute(sql, execute=True)
+
+    async def select_all_users(self):
+        sql = "SELECT * FROM Users"
+        return await self.execute(sql, fetch=True)
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        sql += " AND ".join([
+            f"{item} = ${num}" for num, item in enumerate(parameters.keys(), start=1)
+        ])
+        return sql, tuple(parameters.values())
+
+    async def select_user(self, **kwargs):
+        sql = "SELECT * FROM Users WHERE "
+        sql, parameters = self.format_args(sql, parameters=kwargs)
+        return await self.execute(sql, *parameters, fetchrow=True)
+
+    async def count_users(self):
+        sql = "SELECT COUNT(*) FROM Users"
+        return await self.execute(sql, fetchval=True)
+
+    async def update_user_username(self, username, telegram_id):
+        # SQL_EXAMPLE = "UPDATE Users SET username=$1 WHERE telegram_id=$2"
+        sql = "UPDATE Users SET username=$1 WHERE telegram_id=$2"
+        return await self.execute(sql, username, telegram_id, execute=True)
+
+    async def delete_users(self):
+        await self.execute("DELETE FROM Users WHERE TRUE")
+
+    async def drop_users(self):
+        await self.execute("DROP TABLE Users", execute=True)
